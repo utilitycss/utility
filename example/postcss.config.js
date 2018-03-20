39bbc4145e7a79181da64916b8b7dcc0db8c79d3
Sylvester Aswin Stanley <me@sylvesteraswin.com>
2018-03-20 01:17:08 +0100

    Implement forceInsert logic for utility plugin

--- example/postcss.config.js
+++ example/postcss.config.js
@@ -1,6 +1,6 @@
-const { builder: utility } = require('../src/index');
-const utilityConfig = require('./example.config');
+const { builder: utility } = require("../src/index");
+const utilityConfig = require("./example.config");
 
 module.exports = {
-  plugins: [utility(utilityConfig)],
+  plugins: [utility(utilityConfig)]
 };
