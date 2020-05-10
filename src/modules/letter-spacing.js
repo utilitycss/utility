f6be121622ca1bd8ab79e84e1912b57a3bb5f842
Sylvester Aswin Stanley <sylvester.aswin.stanley@zalando.de>
2020-05-10 20:36:02 +0200

    feat: add support for more utilities

---
+++ src/modules/letter-spacing.js
@@ -0,0 +1,25 @@
+const applyRules = require("../util/applyRules");
+
+const defaultNames = {
+  lts: "lts",
+  "lts-n": "lts-n"
+};
+
+const getRules = (names, cfg) => ({
+  lts: { name: names["lts"], key: "letter-spacing", value: cfg.values },
+  "lts-n": { name: names["lts-n"], key: "letter-spacing", value: "normal" }
+});
+
+const meta = {
+  module: "letter-spacing"
+};
+
+module.exports = config => globalConfig => {
+  return applyRules({
+    config,
+    globalConfig,
+    defaultNames,
+    getRules,
+    meta: Object.assign({}, meta, config && config.meta)
+  });
+};
