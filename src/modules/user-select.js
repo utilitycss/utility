3834b5cd745632dad422bd4ad3f5a700ec9c34a4
Sylvester Aswin Stanley <sylvester.aswin.stanley@zalando.de>
2020-05-11 12:49:27 +0200

    add predefined user-select

--- src/modules/user-select.js
+++ src/modules/user-select.js
@@ -1,11 +1,19 @@
 const applyRules = require("../util/applyRules");
 
 const defaultNames = {
-  us: "us"
+  "us:n": "us:n",
+  "us:au": "us:au",
+  "us:t": "us:t",
+  "us:c": "us:c",
+  "us:a": "us:a"
 };
 
-const getRules = (names, cfg) => ({
-  us: { name: names["us"], key: "user-select", value: cfg.values }
+const getRules = names => ({
+  "us:n": { name: names["us:n"], key: "user-select", value: "none" },
+  "us:au": { name: names["us:au"], key: "user-select", value: "auto" },
+  "us:t": { name: names["us:t"], key: "user-select", value: "text" },
+  "us:c": { name: names["us:c"], key: "user-select", value: "contain" },
+  "us:a": { name: names["us:a"], key: "user-select", value: "all" }
 });
 
 const meta = {
