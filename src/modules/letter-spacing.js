4a2c9f22ead6329238a3e477fb0b4de6a3843d3a
Sylvester Aswin Stanley <sylvester.aswin.stanley@zalando.de>
2020-05-11 10:29:08 +0200

    update letter spacing none

--- src/modules/letter-spacing.js
+++ src/modules/letter-spacing.js
@@ -2,12 +2,12 @@ const applyRules = require("../util/applyRules");
 
 const defaultNames = {
   lts: "lts",
-  "lts-n": "lts-n"
+  "lts:n": "lts:n"
 };
 
 const getRules = (names, cfg) => ({
   lts: { name: names["lts"], key: "letter-spacing", value: cfg.values },
-  "lts-n": { name: names["lts-n"], key: "letter-spacing", value: "normal" }
+  "lts:n": { name: names["lts:n"], key: "letter-spacing", value: "normal" }
 });
 
 const meta = {
